ae64c09d8fef2c5f62dd5019c9a955f086f46292
MichalS16 <104347637+MichalS16@users.noreply.github.com>
2022-09-20 19:10:51 +0200

    Add files via upload

---
+++ ASCII_art/main.py
@@ -0,0 +1,13 @@
+"""
+User interface section of ASCII_art program
+
+How to ASCII generator: run this file
+
+created by: Michal S.
+"""
+
+# import UI and generator of ASCII
+from user_interface import *
+
+if __name__ == "__main__":
+    run()
